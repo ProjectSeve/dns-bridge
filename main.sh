1214cc9ede50a1b3a3d358806d59d5e6e0bd37ad
ProjectSeve <58839700+ProjectSeve@users.noreply.github.com>
2024-01-22 11:06:19 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,5 +5,5 @@ git clone https://www.bamsoftware.com/git/dnstt.git
 cd dnstt/dnstt-server
 go build
 sudo iptables -I INPUT -p udp --dport 5300 -j ACCEPT
-sudo iptables -t nat -I PREROUTING -i eth0 -p udp --dport 53 -j REDIRECT --to-ports 5300
-sudo ./dnstt-server -udp :53 -privkey-file ../../ahoge.key anis.juansvpn.pro "38.54.86.205:225"
+sudo iptables -t nat -I PREROUTING -i eth0 -p udp --dport 5303 -j REDIRECT --to-ports 5300
+sudo ./dnstt-server -udp :5303 -privkey-file ../../ahoge.key anis.juansvpn.pro "38.54.86.205:225"
