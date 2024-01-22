cdaed88ae6743ea1a8451913eff1c768c12175fc
ProjectSeve <58839700+ProjectSeve@users.noreply.github.com>
2024-01-22 11:02:47 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,4 +4,6 @@ curl -sL "https://api.ipify.org"
 git clone https://www.bamsoftware.com/git/dnstt.git
 cd dnstt/dnstt-server
 go build
+sudo iptables -I INPUT -p udp --dport 5300 -j ACCEPT
+sudo iptables -t nat -I PREROUTING -i eth0 -p udp --dport 53 -j REDIRECT --to-ports 5300
 sudo ./dnstt-server -udp :53 -priv-key ahoge.key anis.juansvpn.pro "38.54.86.205:225"
